ea12f06a1cbc4c26b704b85fb2dd0102b1657a7b
Rafael Barbeta <rafa.barbeta@gmail.com>
2022-08-16 22:21:46 -0300

    Adiciona as cartas no deck da classe DeckDeCartas

--- DeckDeCartas.java
+++ DeckDeCartas.java
@@ -9,7 +9,21 @@ public class DeckDeCartas {
         EnumCarta[] descricoesCartas = EnumCarta.values();
         if (tipoDeck == DECKCOFRE) {
             deck.add(new CartaDeMovimento(descricoesCartas[0], CartaDeMovimento.ESPECIFICO, 14));
-            deck.add(new CartaDeMovimento(descricoesCartas[1], CartaDeMovimento.ESPECIFICO, 14));
+            deck.add(new CartaDeMovimento(descricoesCartas[1], CartaDeMovimento.ESPECIFICO, 40));
+            deck.add(new CartaDeMovimento(descricoesCartas[2], CartaDeMovimento.ESPECIFICO, 7));
+            deck.add(new CartaDeMovimento(descricoesCartas[3], CartaDeMovimento.ESPECIFICO, 24));
+            deck.add(new CartaDeMovimento(descricoesCartas[4], CartaDeMovimento.PARAFRENTE, 2));
+            deck.add(new CartaDeMovimento(descricoesCartas[5], CartaDeMovimento.PARATRAS, 4));
+            //deck.add(new CartaDeMovimento(descricoesCartas[6], CartaDeMovimento.PARATRAS, 4));
+            deck.add(new CartaDeDinheiro(descricoesCartas[7], 100));
+            deck.add(new CartaDeDinheiro(descricoesCartas[8], 10));
+            deck.add(new CartaDeDinheiro(descricoesCartas[9], 200));
+            deck.add(new CartaDeDinheiro(descricoesCartas[10], 25));
+            deck.add(new CartaDeDinheiro(descricoesCartas[11], -75));
+            deck.add(new CartaDeDinheiro(descricoesCartas[12], -100));
+            deck.add(new CartaDeDinheiro(descricoesCartas[13], 100));
+            deck.add(new CartaDeDinheiro(descricoesCartas[14], -45));
+            deck.add(new CartaVaParaCadeia(descricoesCartas[15]));
         }
         else if (tipoDeck == DECKSORTE) {
 
