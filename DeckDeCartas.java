cedb9c2485850cbea94cfa59235b21913e7be7d1
Rafael Barbeta <rafa.barbeta@gmail.com>
2022-08-19 10:04:35 -0300

    Correção falta de inicialização ArrayList DeckDeCartas

--- DeckDeCartas.java
+++ DeckDeCartas.java
@@ -16,6 +16,7 @@ public class DeckDeCartas {
      */
     public DeckDeCartas(int tipoDeck) {
         EnumCarta[] descricoesCartas = EnumCarta.values();
+        deck = new ArrayList<Carta>();
         if (tipoDeck == DECKCOFRE) {
             deck.add(new CartaDeMovimento(descricoesCartas[0], CartaDeMovimento.ESPECIFICO, 14));
             deck.add(new CartaDeMovimento(descricoesCartas[1], CartaDeMovimento.ESPECIFICO, 40));
