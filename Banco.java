a747e3f6f865da349220e2520f39b5b3a8eb7d1c
beatrizpat <beatrizpatriciost@gmail.com>
2022-09-06 23:14:13 -0300

    add documentação

--- Banco.java
+++ Banco.java
@@ -1,7 +1,9 @@
+/* 
+ * Classe responsável por controlar todas as operações monetárias do jogo.
+ */
 public class Banco {
 
     public Banco() {
-
     }
 
     /**
@@ -14,50 +16,57 @@ public class Banco {
     }
     
     /**
+     * @return Verdadeiro quando o pagamento foi realizado, se não, o jogador pagador entrou em falência para o Banco.
      * @param pagador O jogador que irá realizar o pagamento ao Banco.
      * @param valor O valor a ser recebido pelo banco e descontado do saldo do Jogador.
      */
-    public void pagarBanco(Jogador pagador, int valor) { //comprou propriedade, casa, pagou taxa, imposto
-        int valorAtual = pagador.getSaldo();
-        if(valorAtual < valor)
-            detectouFalencia(pagador);
-        else {
-            pagador.setSaldo(valorAtual - valor);      
+    public boolean pagarBanco(Jogador pagador, int valor) { //comprou propriedade, casa, pagou taxa, imposto
+        int valorAtualPagador = pagador.getSaldo();
+        if(valorAtualPagador < valor) {
+            detectouFalencia(pagador); //troca propriedades
+            return false;
+        } else {
+            pagador.setSaldo(valorAtualPagador - valor);
+            return true;      
         }
     }
     
     /**
+     * @return Verdadeiro quando o pagamento foi realizado, se não, o jogador pagador entrou em falência para o jogador recebedor.
      * @param pagador O jogador que irá realizar um pagamento a outro.
      * @param recebedor O jogador que irá receber um pagamento de outro.
      * @param valor O valor que será repassado do pagador para o recebedor.
      */
-    public void pagamentoEntreJogadores(Jogador pagador, Jogador recebedor, int valor) {
-        int valorAtual = pagador.getSaldo();
-        pagador.setSaldo(valorAtual - valor);
-    
-        valorAtual = recebedor.getSaldo();
-        recebedor.setSaldo(valorAtual + valor);
+    public boolean pagamentoEntreJogadores(Jogador pagador, Jogador recebedor, int valor) {
+        int valorAtualPagador = pagador.getSaldo();
+        if(valorAtualPagador < valor) {
+            detectouFalencia(pagador, recebedor); //troca propriedades
+            return false;
+        } else {
+            pagador.setSaldo(valorAtualPagador - valor);
+            recebedor.setSaldo(recebedor.getSaldo() + valor);
+            return true;
+        }
     }
+
     /**
      * @param pagador O jogador que entrou em falência devendo para o Banco.
      */
-    public boolean detectouFalencia(Jogador pagador) {
-        saldoBanco = saldoBanco + pagador.getSaldo();
-        for(Espaco p: pagador.conjuntoPropriedades) { //teria que ser do tipo Propriedade??
-            p.setDono(null); //propriedades disponiveis pra compra
+    public void detectouFalencia(Jogador pagador) {
+        for(Espaco p: pagador.getConjunPropriedades()) { //teria que ser do tipo Propriedade??
+            ((Propriedade) p).setDono(null); //propriedades disponiveis pra compra
         }
     }
     /**
      * @param pagador O jogador que entrou em falência e irá perder as propriedades.
      * @param recebedor O jogador que receberá as propriedades do jogador em falência.
      */
-    public boolean detectouFalencia(Jogador pagador, Jogador Recebedor) {
-        for(Propriedade p: pagador.conjuntoPropriedades) {
-            p.setDono(Recebedor); //passa para o Recebedor (adicionarPropriedade já faz isso?)
-            Recebedor.adicionarPropriedade(p);
-        }
+    public void detectouFalencia(Jogador pagador, Jogador Recebedor) {
+        //A TROCA DE PROPIEDADES É FEITA PELO JOGO?
+        //for(Propriedade p: pagador.conjuntoPropriedades) {
+            //p.setDono(Recebedor); //passa para o Recebedor (adicionarPropriedade já faz isso?)
+            //Recebedor.adicionarPropriedade(p);
+        //}
     }
-    /*fazer um if antes de todas as operações de pagamento pra detectar falência. se entrar, chamar um método do jogo que "desconecta" jogador?
-    ou fazer um retorno, se retornar 1 no jogo realizou normal, se não, o jogador n tinha saldo e declara em falencia
-    */
+
 }
