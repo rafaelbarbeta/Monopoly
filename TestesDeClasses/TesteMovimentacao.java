ec30c20869df4cd4ec647626b246d51cff4f0fff
beatrizpat <beatrizpatriciost@gmail.com>
2022-09-06 23:16:18 -0300

    add teste movimento para mais proximo

--- TestesDeClasses/TesteMovimentacao.java
+++ TestesDeClasses/TesteMovimentacao.java
@@ -1,11 +1,15 @@
 public class TesteMovimentacao {
     public static void main(String[] args) {
         Tabuleiro tabuleiro = new Tabuleiro();
+        CartaDeMovimento cartaTeste1 =new CartaDeMovimento(EnumCarta.COFRE_07, CartaDeMovimento.MAISPROXIMO, CartaDeMovimento.UTILIDADE);
+        CartaDeMovimento cartaTeste2 =new CartaDeMovimento(EnumCarta.SORTE_07, CartaDeMovimento.MAISPROXIMO, CartaDeMovimento.METRO);
         Jogador j1 = new Jogador("Rafael", tabuleiro.getTabuleiro()[1]);
+        Jogador j2 = new Jogador("Beatriz", tabuleiro.getTabuleiro()[37]); 
         Dado dado1 = new Dado();
         Dado dado2 = new Dado();
         int valDado1,valDado2;
         boolean passouPeloInicio;
+        
         for (int i = 0; i < 10; i++) {
             System.out.println(j1.getNome() +" esta: " + j1.getLocalizacao().getNome());
             valDado1 = dado1.rolar();
@@ -16,6 +20,10 @@ public class TesteMovimentacao {
                 System.out.println(j1.getNome() + " passou pelo inicio!");
             }
         }
-
+        
+        System.out.println();
+        System.out.println(j2.getNome() + " esta: "+ j2.getLocalizacao().getNome());
+        tabuleiro.movimentarJogador(j2, cartaTeste2);
+        System.out.println(j2.getNome() +" foi pra: "+ j2.getLocalizacao().getNome());
     }
 }
